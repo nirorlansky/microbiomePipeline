7b776997a0157af728ded31a7e66b2da5dff9a1a
nirorlansky <orlansky.nir@gmail.com>
2025-09-06 19:37:08 +0300

    moved scores to global vars

--- src/pkg/globals.py
+++ src/pkg/globals.py
@@ -1,3 +1,9 @@
 HEALTHY = 0
 SICK = 1
 #
+SCORES = {
+        "roc_auc": "roc_auc",
+        "aupr": "average_precision",  # FIXED: use built-in string scorer
+        "f1": "f1",
+        "acc": "accuracy",
+    }
