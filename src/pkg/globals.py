0a72e1f176002bee5b7ddc82a1285964881e60ab
nirorlansky <orlansky.nir@gmail.com>
2025-09-08 20:40:11 +0300

    small mistake fix

--- src/pkg/globals.py
+++ src/pkg/globals.py
@@ -4,6 +4,6 @@ SICK = 1
 SCORES = {
         "roc_auc": "roc_auc",
         "aupr": "average_precision",  # FIXED: use built-in string scorer
-        "f1": "f1",
+        "recall": "recall",
         "acc": "accuracy",
     }
